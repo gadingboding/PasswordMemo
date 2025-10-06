418bd8a057294300a96fd5b3cb15a71beaa70b29
aana <aa@123.com>
2025-10-06 22:04:21 +0800

    修改配置

--- password-manager-extension/vite.config.ts
+++ password-manager-extension/vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
       input: {
         popup: resolve(__dirname, 'popup.html'),
         options: resolve(__dirname, 'options.html'),
-        content: resolve(__dirname, 'src/content.ts'),
-        background: resolve(__dirname, 'src/background.ts')
+        content: resolve(__dirname, 'src/content.ts')
       },
       output: {
         entryFileNames: '[name].js',
